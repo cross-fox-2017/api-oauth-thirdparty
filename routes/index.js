54596f2ffc62b38704914b8e871cce24a84ce276
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-02-04 17:52:59 +0700

    routing index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var userController = require('../controllers/user')
+var passport = require('passport')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('OK')
-});
+router.post('/signup', userController.signupUser)
+router.post('/signin', userController.signinUser)
 
-module.exports = router;
+router.get('/facebook', passport.authenticate('facebook'))
+
+router.get('/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/home',
+  failureRedirect: '/out'
+}))
+
+router.get('/home', function (req, res) {
+  res.send('Welcome home !')
+})
+
+router.get('/out', function (req, res) {
+  res.send('Who the %^&*( are you ! get OUT !!!')
+})
+module.exports = router
