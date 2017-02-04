fbfdd0ee2492b1b2218d4e966429d233edd512c6
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-04 13:40:31 +0700

    Create Routing

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
+require('dotenv').config()
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-require('dotenv').config()
-
 const mongoose = require('mongoose');
 const passport = require('passport');
 const flash = require('connect-flash');
@@ -16,10 +14,6 @@ const session = require('express-session');
 mongoose.Promise = global.Promise;
 const configDB = require('./config/database.js');//connect to mongoDB
 mongoose.connect(configDB.url)
-
-
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -35,16 +29,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+
 
 //required for passport
-app.use(session({secret: proscess.env.SESSION_SECRET}))
+app.use(session({secret: process.env.SESSION_SECRET}))
 app.use(passport.initialize())
 app.use(passport.session())//persistent login sessions
 app.use(flash());//use connect-flash for flash message stored in session
 
-require('./app/routes.js')(app,passport)
+
+// var index = require('./routes/index');
+// var users = require('./routes/users');
+// app.use('/', index);
+// app.use('/users', users);
+require('./app/routes.js')(app, passport)//proses routing dipindah ke folder app/routes.js, jika empat sintak diatas diaktifkan, maka routing akan di tulis di rotes
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ app/routes.js
@@ -0,0 +1,33 @@
+module.exports = function(app, passport) {
+
+    app.get('/', function(req, res, next) {
+        res.render('index.ejs', {
+            title: "Testing"
+        })
+    })
+
+    app.get('/login', function(req, res, next) {
+        res.render('login.ejs', {
+            message: req.flash('loginMessage')
+        })
+    })
+
+    app.get('/signup', function(req, res, next) {
+        res.render('signup.ejs', {
+          message: req.flash('signupMessage')
+        })
+    })
+
+    app.get('/profile',isLoggedIn, function(req, res, next) {
+      res.sender('profile.ejs',{
+        user: req.user
+      })
+    })
+
+    app.get('logout',function(req, res, next) {
+      req.logout();
+      res.redirect('/')
+    })
+
+
+}
