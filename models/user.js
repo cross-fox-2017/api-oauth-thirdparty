329f4a01ef3efc73414377b40b8657776871110f
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-02-04 17:52:17 +0700

    models

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  email: String,
+  token: String,
+  facebook: {
+    id: String,
+    token: String,
+    name: String
+  },
+  twitter: {
+    id: String,
+    token: String,
+    displayName: String,
+    username: String
+  },
+  google: {
+    id: String,
+    token: String,
+    email: String,
+    name: String
+  }
+},
+  {
+    timestamps: true
+  })
+
+module.exports = mongoose.model('Users', UserSchema)
