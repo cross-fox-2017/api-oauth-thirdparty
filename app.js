a482e9a773ab0a6d10c9f1dec461d18cb8aeb7d5
yomaswn <yaumashf@gmail.com>
2017-02-07 15:24:16 +0700

    installed all pass

---
+++ app.js
@@ -0,0 +1,161 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var index = require('./routes/index');
+var users = require('./routes/users');
+var passport = require('passport')
+var app = express();
+//model require
+var User = require('./models/user.js')
+app.use(passport.initialize())
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(passport.session());
+//strategies
+var FacebookStrategy = require('passport-facebook').Strategy
+var TwitterStrategy = require('passport-twitter').Strategy
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+
+passport.serializeUser(function(user,done){
+  done(null,user.id)
+})
+passport.deserializeUser(function(id,done){
+  User.findById(id,function(err,user){
+    done(err,user)
+  })
+})
+//facebook-oauth
+passport.use(new FacebookStrategy({
+    clientID: '150196128819994',
+    clientSecret: '58ada52c316d364628e21caa21d93e57',
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function(){
+	    		User.findOne({'facebook.id': profile.id}, function(err, user){
+	    			if(err)
+	    				return done(err);
+	    			if(user)
+	    				return done(null, user);
+	    			else {
+	    				var newUser = new User();
+	    				newUser.facebook.id = profile.id;
+	    				newUser.facebook.token = accessToken;
+	    				newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
+
+
+	    				newUser.save(function(err){
+	    					if(err)
+	    						throw err;
+	    					return done(null, newUser);
+	    				})
+	    				console.log(profile);
+	    			}
+	    		});
+	    	});
+  }
+));
+//twitter-oauth
+passport.use(new TwitterStrategy({
+    consumerKey: 'jIrcD2fFilY175sFeNf2DzAPM',
+    consumerSecret: '7WBdYizFIlTk1egoDy1Z8GNcSJ1JZvtanXPuhDF49RacCkFYRX',
+    callbackURL: "http://localhost:3000/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    process.nextTick(function(){
+	    		User.findOne({'twitter.id': profile.id}, function(err, user){
+	    			if(err)
+	    				return done(err);
+	    			if(user)
+	    				return done(null, user);
+	    			else {
+	    				var newUser = new User();
+	    				newUser.twitter.id = profile.id;
+	    				newUser.twitter.token = token;
+	    				newUser.twitter.name = profile.username;
+
+	    				newUser.save(function(err){
+	    					if(err)
+	    						throw err;
+	    					return done(null, newUser);
+	    				})
+	    				console.log(profile);
+	    			}
+	    		});
+	    	});
+  }
+));
+
+//google-oauth
+passport.use(new GoogleStrategy({
+    clientID: '950493030103-ug59dkhnh9p7kqlg9gc42ru2o2ihrcuk.apps.googleusercontent.com',
+    clientSecret: 'PBcmmyXVVxpT3API581xyFzW',
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    process.nextTick(function(){
+	    		User.findOne({'google.id': profile.id}, function(err, user){
+	    			if(err)
+	    				return done(err);
+	    			if(user)
+	    				return done(null, user);
+	    			else {
+	    				var newUser = new User();
+	    				newUser.google.id = profile.id;
+	    				newUser.google.token = token;
+	    				newUser.google.name = profile.displayname;
+
+	    				newUser.save(function(err){
+	    					if(err)
+	    						throw err;
+	    					return done(null, newUser);
+	    				})
+	    				console.log(profile);
+	    			}
+	    		});
+	    	});
+  }
+));
+
+//connect mongoose
+mongoose.connect('mongodb://localhost/oauth')
+mongoose.Promise = global.Promise
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var userSchema = new Schema({
+  name:String,
+  email:String,
+  username:String,
+  password:String,
+  facebook:{
+    id:String,
+    token:String,
+    name:String
+  },
+  twitter:{
+    id:String,
+    token:String,
+    name:String
+  },
+  google:{
+    id:String,
+    token:String,
+    name:String
+  },
+  github:{
+    id:String,
+    token:String,
+    name:String
+  }
+});
+
+var User = mongoose.model('users',userSchema)
+
+module.exports = User
