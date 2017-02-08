84833ed8e59a2c2a66a449c7e202496891ca7f6c
qblol <iqbal.0809@gmail.com>
2017-02-08 21:44:16 +0700

    done setelah berkali-kali ngulang

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'facebookAuth' : {
+    'clientID'          :  '1341010272603781',
+    'clientSecret'      :  'b547be5d70f1fe79c8b3ee5e5ce1ae69',
+    'callbackURL'       : 'http://localhost:3000/auth/facebook/callback'
+  },
+  'twitterAuth' : {
+    'consumerKey'       : 'YXywtIpx54Ev5izDoXEz4xNG2',
+    'consumerSecret'    : 'E8f07JNRKpBVzwtAknwuwHnWKhZlhaAeAfOLIIKPLYib1qD23C',
+    'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
+  },
+  'googleAuth' : {
+    'clientID'          : '514835205312-1li4e56cmeod3v50vhf7lrv0em263p1o.apps.googleusercontent.com',
+    'clientSecret'      : 'UBOdrM6lFj2otP2BVjoOVAY7',
+    'callbackURL'       : 'http://localhost:3000/auth/google/callback'
+  },
+  'localAuth' : {
+    'secret'            : 'uvuvwevwevwe onyetenyevwe ugwebmubwem osas'
+  },
+};
